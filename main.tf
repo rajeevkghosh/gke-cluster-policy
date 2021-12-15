1dcf0d9737d1b34e83777c26c7b30974c8b1ed21
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-12-15 22:17:19 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,31 @@
 provider "google" {
     project = "airline1-sabre-wolverine"
-    #credentials = file("../gke.json")
+    credentials = file("../gke.json")
 }
 resource "google_container_cluster" "primary" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
-  #datapath_provider = "LEGACY_DATAPATH"
+  datapath_provider = "ADVANCED_DATAPATH"
 
   ip_allocation_policy {
 
   }
-  /*release_channel {
-  channel = "REGULAR"
-  }*/
-  /*master_authorized_networks_config {
+
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes = true
+
+    master_ipv4_cidr_block = "192.168.10.16/28"
+  }
+  release_channel {
+  channel = "STABLE"
+  }
+  master_authorized_networks_config {
       cidr_blocks {
           cidr_block = "192.168.10.35/32"
       }
-  }*/
+  }
   node_config {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     #service_account = google_service_account.default.email
